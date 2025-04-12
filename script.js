9daec016b61a9d92ca636bf7e538934cf1efec09
Sujoy Sadhukhan <alpha9339321@gmail.com>
2025-04-13 03:15:49 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const manu = document.querySelector(".mENU_lINKS");
+    const icon = document.querySelector(".hAMBURGER_iCON");
+    manu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
